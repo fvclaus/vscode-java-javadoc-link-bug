60b1c95ecb46fca263f28a94ba7c794446f95b66
clausfr <claus.hays@partner.akdb.de>
2021-07-21 12:58:02 +0200

    Initial commit

---
+++ src/main/java/fvclaus/App.java
@@ -0,0 +1,14 @@
+package fvclaus;
+
+import java.util.HashSet;
+
+/**
+ * Uses {@code HashSet#clear()}
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/fvclaus/AppTest.java
@@ -0,0 +1,20 @@
+package fvclaus;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertThat(true, equalTo(false));
+    }
+}
